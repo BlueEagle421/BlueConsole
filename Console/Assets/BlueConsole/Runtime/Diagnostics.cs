2589e4e4eecc53c071f3edf1245f348463abc8e3
BlueEagle421 <joachim.wielewski@gmail.com>
2023-12-05 17:33:35 +0100

    Add diagnostics hardware type logging

--- Console/Assets/BlueConsole/Runtime/Diagnostics.cs
+++ Console/Assets/BlueConsole/Runtime/Diagnostics.cs
@@ -5,9 +5,29 @@ public class Diagnostics : MonoBehaviour
     [SerializeField] private RectTransform _diagnosticsGUIParent;
     public static bool IsDiagnosing { get; private set; }
 
-    [Command("diagnose", "toggles fps, hwinfo and usage")]
-    public static void Diagnose(bool on)
+    private void Start()
     {
+        if (IsDiagnosing)
+            ToggleDiagnostics(true);
+    }
 
+    private void ToggleDiagnostics(bool toggle)
+    {
+        if (!IsDiagnosing && toggle)
+            ToggledOn();
+
+        IsDiagnosing = toggle;
+    }
+
+    private void ToggledOn()
+    {
+        Debug.Log("CPU type: " + SystemInfo.processorType);
+        Debug.Log("GPU type: " + SystemInfo.graphicsDeviceName);
+    }
+
+    [Command("diagnose", "toggles fps, hwinfo and usage", InstanceTargetType.First)]
+    public void Diagnose(bool on)
+    {
+        ToggleDiagnostics(on);
     }
 }
