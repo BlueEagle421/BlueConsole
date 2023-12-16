5ba041710304e32ffdcd2713f769e39eaebb41e4
BlueEagle421 <joachim.wielewski@gmail.com>
2023-12-17 00:04:22 +0100

    Rename ConsoleController to ConsoleVisuals

--- Console/Assets/BlueConsole/Runtime/ConsoleVisuals.cs
+++ Console/Assets/BlueConsole/Runtime/ConsoleVisuals.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 using UnityEngine.EventSystems;
 using UnityEngine.UI;
 
-public class ConsoleController : MonoBehaviour
+public class ConsoleVisuals : MonoBehaviour
 {
     [SerializeField] private float _height;
     [SerializeField] private float _scale;
