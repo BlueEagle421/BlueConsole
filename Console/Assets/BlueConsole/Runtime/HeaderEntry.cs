b3cff4f299488d5ce9503b5de9c509e8e6f45080
BlueEagle421 <joachim.wielewski@gmail.com>
2024-02-27 19:51:11 +0100

    Add Priority to HeaderEntry

--- Console/Assets/BlueConsole/Runtime/HeaderEntry.cs
+++ Console/Assets/BlueConsole/Runtime/HeaderEntry.cs
@@ -1,13 +1,27 @@
 using System;
+using UnityEngine;
 
 public class ConsoleHeaderEntry
 {
     public Func<string> LabelFunc;
-    public Func<UnityEngine.Color> ColorFunc;
+    public Func<Color> ColorFunc;
 
-    public ConsoleHeaderEntry(Func<string> labelFunc, Func<UnityEngine.Color> colorFunc)
+    public int Priority
+    {
+        get
+        {
+            return Priority;
+        }
+        set
+        {
+            Mathf.Max(0, value);
+        }
+    }
+
+    public ConsoleHeaderEntry(Func<string> labelFunc, Func<Color> colorFunc, int priority)
     {
         LabelFunc = labelFunc;
         ColorFunc = colorFunc;
+        Priority = priority;
     }
 }
