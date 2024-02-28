ab3a47e871a375fa1a82650210dd61ab41a15c38
BlueEagle421 <joachim.wielewski@gmail.com>
2024-02-28 11:24:52 +0100

    Fix HeaderEntry properties

--- Console/Assets/BlueConsole/Runtime/HeaderEntry.cs
+++ Console/Assets/BlueConsole/Runtime/HeaderEntry.cs
@@ -7,16 +7,19 @@ public class HeaderEntry
     public Func<string> LabelFunc;
     public Func<Color> ColorFunc;
 
+    private int _width;
+    private int _priority;
+
     public int Width
     {
         get
         {
-            return Width;
+            return _width;
         }
         set
         {
             //if width is 0 it will be ignored and set to prefab's default
-            Mathf.Max(0, value);
+            _priority = Mathf.Max(0, value);
         }
     }
 
@@ -24,12 +27,12 @@ public class HeaderEntry
     {
         get
         {
-            return Priority;
+            return _priority;
         }
         set
         {
             //if priority is 0 it will be ignored and added as the last one
-            Mathf.Max(0, value);
+            _priority = Mathf.Max(0, value);
         }
     }
 
